5b11e13c1c702994e56ce060429a939838e23be6
Beer Florian <Florian.Beer@jp.bosch.com>
2022-03-15 10:04:34 +0900

    Add license information

--- SetupLemonTreeIntegration.bat
+++ SetupLemonTreeIntegration.bat
@@ -1,4 +1,7 @@
 @echo off
+rem This Source Code Form is subject to the terms of the Mozilla Public
+rem License, v. 2.0. If a copy of the MPL was not distributed with this
+rem file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 echo Registering LemonTree as merge driver for LFS
 git config merge.lemontree.name "lemontree merge driver"
