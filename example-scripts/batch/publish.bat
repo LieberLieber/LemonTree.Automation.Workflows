f98d041f4d9c7d06cfec7da2d531573f93e284df
paulkronabethleitner <paul.kronabethleitner@lieberlieber.com>
2022-04-05 16:32:49 +0200

    added example scripts

---
+++ example-scripts/batch/publish.bat
@@ -0,0 +1,3 @@
+"C:\Program Files\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe" publish --model EAExample.eap --packagedirectory .\repo --componentguids "{c2791d98-4982-d2dd-449c-271ac0dbf1c2}","f7b4a94e-422c-94ee-1d44-ed33655e8d8e",{96e2af8b-e5e2-4a4f-059c-abc3ef420012} --releasenotes "my notes"
+echo Exitcode %errorlevel%
+pause
