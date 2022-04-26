a7399111e144402489b364f00aae7ee3108c21e0
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 14:31:31 +0200

    Update getLTA.ps1
    Added Download

--- Action/GetLemonTreeAutomation/getLTA.ps1
+++ Action/GetLemonTreeAutomation/getLTA.ps1
@@ -3,5 +3,7 @@ param(
     [parameter(Mandatory = $true)]
     [string]$LemonTreePackageURL
 )
-echo "FOO"
-echo $LemonTreePackageURL
+echo "Download LemonTree.Automtion from Repo"
+while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
+curl "$LemonTreePackageURL" --output LTA.zip -k
+Expand-Archive LTA.zip -DestinationPath ..\LTA\ -Force
