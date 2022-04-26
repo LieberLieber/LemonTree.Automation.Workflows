490673bacd2c3e7a942eb2fd03c91a2b8cffb155
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 15:43:50 +0200

    Hopefully Path is working nbwo

--- Action/GetLemonTreeAutomation/getLTA.ps1
+++ Action/GetLemonTreeAutomation/getLTA.ps1
@@ -2,9 +2,11 @@
 param(
     [parameter(Mandatory = $true)]
     [string]$LemonTreePackageURL
+    [string]$License
 )
 echo "Download LemonTree.Automtion from Repo"
+echo "$License"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
 curl "$LemonTreePackageURL" --output LTA.zip -k
-Expand-Archive LTA.zip -DestinationPath ..\LTA\ -Force
+Expand-Archive LTA.zip -DestinationPath .\LTA\ -Force
 echo '::set-output name=LemonTreeAutomationExecutable::.\LTA\LemonTree.Automation.exe'
