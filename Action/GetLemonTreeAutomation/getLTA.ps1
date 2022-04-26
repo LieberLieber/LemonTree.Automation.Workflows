f823df07ed4c9b0c77e9ee8e1f67abeada3ccce1
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 16:16:49 +0200

    Update getLTA.ps1

--- Action/GetLemonTreeAutomation/getLTA.ps1
+++ Action/GetLemonTreeAutomation/getLTA.ps1
@@ -3,7 +3,7 @@ param(
     [parameter(Mandatory = $true)]
     [string]$LemonTreePackageURL,
 
-    [parameter(Mandatory = $false)]
+    [parameter(Mandatory = $true)]
     [string]$License
 )
 
@@ -12,4 +12,12 @@ echo "***$License**"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
 curl "$LemonTreePackageURL" --output LTA.zip -k
 Expand-Archive LTA.zip -DestinationPath .\LTA\ -Force
+
+IF([string]::IsNullOrWhiteSpace($License)) {            
+    echo "No License provided."         
+} else {            
+    echo "Create License File ${License}"
+    $License | Out-File -FilePath lta.lic #if you deploy the license on the fly
+}  
+
 echo '::set-output name=LemonTreeAutomationExecutable::.\LTA\LemonTree.Automation.exe'
