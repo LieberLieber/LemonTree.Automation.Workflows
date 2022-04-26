d79db6641574ac45c73a503ee0d85113d1b9e771
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 15:53:55 +0200

    Update getLTA.ps1

--- Action/GetLemonTreeAutomation/getLTA.ps1
+++ Action/GetLemonTreeAutomation/getLTA.ps1
@@ -1,12 +1,12 @@
 #getLTA.ps1
 param(
     [parameter(Mandatory = $true)]
-    [string]$LemonTreePackageURL
-)
-param(
+    [string]$LemonTreePackageURL,
+
     [parameter(Mandatory = $false)]
     [string]$License
 )
+
 echo "Download LemonTree.Automtion from Repo"
 echo "***$License**"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
