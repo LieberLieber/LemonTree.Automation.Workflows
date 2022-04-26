e21f35a3ac963d519dffb50fb8824b75e96f7d82
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 15:17:59 +0200

    LTA Executable location should be available in the workflow
    License is still missing

--- Action/GetLemonTreeAutomation/getLTA.ps1
+++ Action/GetLemonTreeAutomation/getLTA.ps1
@@ -7,3 +7,4 @@ echo "Download LemonTree.Automtion from Repo"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
 curl "$LemonTreePackageURL" --output LTA.zip -k
 Expand-Archive LTA.zip -DestinationPath ..\LTA\ -Force
+echo '::set-output name=LemonTreeAutomationExecutable::"LTA\\LemonTree.Automation.exe"'
