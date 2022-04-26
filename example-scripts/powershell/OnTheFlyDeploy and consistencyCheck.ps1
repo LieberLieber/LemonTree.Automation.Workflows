7e33701549c40317f6137d28daa5d4f3dd469536
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 11:27:42 +0200

    Add Powershell Script to AutoDeploy LTA from .zip

--- example-scripts/powershell/OnTheFlyDeploy and consistencyCheck.ps1
+++ example-scripts/powershell/OnTheFlyDeploy and consistencyCheck.ps1
@@ -1,4 +1,8 @@
 #Powershell sees curly brackets as codeblocks so use quotations marks "{}" or just write your guids without them
-C:\"Program Files"\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe ConsistencyCheck --Model .\..\..\DemoModel.eapx
+#assume license is in appdata
+while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
+curl "https://nexus.lieberlieber.com/repository/lemontree-release/LemonTree.Automation/LemonTree.Automation.Zip_Deploy.zip" --output LTA.zip -k
+Expand-Archive LTA.zip -DestinationPath ..\LTA\ -Force
+..\LTA\LemonTree.Automation.exe ConsistencyCheck --Model .\..\..\DemoModel.eapx #--License lta.lic if you deploy the license on the fly
 echo Exitcode $LASTEXITCODE
 pause
