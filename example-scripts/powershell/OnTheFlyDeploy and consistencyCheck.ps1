8f9b43bbe3cd3593c76d7a1e5a163312827713e3
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-26 12:26:47 +0200

    Update OnTheFlyDeploy and consistencyCheck.ps1
    updated to single quotes so it works with RLM License Info containing double quotes

--- example-scripts/powershell/OnTheFlyDeploy and consistencyCheck.ps1
+++ example-scripts/powershell/OnTheFlyDeploy and consistencyCheck.ps1
@@ -3,7 +3,7 @@
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
 curl "https://nexus.lieberlieber.com/repository/lemontree-release/LemonTree.Automation/LemonTree.Automation.Zip_Deploy.zip" --output LTA.zip -k
 Expand-Archive LTA.zip -DestinationPath ..\LTA\ -Force
-#"<license string goes here>" | Out-File -FilePath lta.lic #if you deploy the license on the fly
+#'<license string goes here>' | Out-File -FilePath lta.lic #if you deploy the license on the fly
 ..\LTA\LemonTree.Automation.exe ConsistencyCheck --Model .\..\..\DemoModel.eapx #--License lta.lic if you deploy the license on the fly
 echo Exitcode $LASTEXITCODE
 pause
