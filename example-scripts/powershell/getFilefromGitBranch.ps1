40aefc4b210d4eb95fa5fea4ca17d9b696901dfb
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-29 11:37:20 +0200

    this way works better

---
+++ example-scripts/powershell/getFilefromGitBranch.ps1
@@ -0,0 +1,17 @@
+﻿$filename = "..\..\DemoModel.eapx"
+$compareToBranch ="main"
+
+$currentBranch = git branch --show-current
+$baseId = git merge-base $compareToBranch $currentBranch
+echo $baseId
+$gitUri =  git config --get remote.origin.url
+echo $gitUri
+$gitUri = $gitUri.Replace(".git","");
+echo $gitUri
+$tempFilename = [System.IO.Path]::GetTempFileName() +".eapx"
+echo $tempFilename
+
+echo "Downloading from $url"
+while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
+curl "$url" --output "$tempFilename" -L -k #-L follows the redirect to get the LFS file.
+
