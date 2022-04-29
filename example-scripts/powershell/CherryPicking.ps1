9979d108b349cddb984dbd7ba32ea72e0724388d
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-29 20:41:27 +0200

    Complete Rewrite

--- example-scripts/powershell/CherryPicking.ps1
+++ example-scripts/powershell/CherryPicking.ps1
@@ -1,20 +1,16 @@
 ﻿#CherryPicking.ps1
+#Author: Daniel Siegl, LieberLieber Software GmbH
+#The purpose of this script is to start lemontree with a model from different branch in cherry pick mode!
+#Attention: the base and branchcopy of the file are collected from origin!
 #Supported:
 #.\example-scripts\powershell\CherryPicking.ps1 -Model DemoModel.eapx -Branch 70-test-for-discussion
 
-
-# Currently not supported - script needs to be run next to the model.
-#.\CherryPicking.ps1 -Model ..\..\DemoModel.eapx
-#.\CherryPicking.ps1 -Model C:\GitHub\LemonTree.Automation.Workflows\DemoModel.eapx 
-
-#Comment this in to run in x86 space with Jet Driver
-#$script = Start-Job -ScriptBlock {
-
-    param
-    (
-        [parameter(Mandatory = $true)][string]$Model,
-        [parameter(Mandatory = $true)][string]$Branch
-    )
+param
+(
+        [Parameter(Mandatory = $false)][string] $filename = "..\DemoModel.eapx",
+        [Parameter(Mandatory = $false)][string] $compareToBranch = "70-test-for-discussion",
+        [Parameter(Mandatory = $false)][boolean] $conflicedFilter = 1 #if set to 1 it will add conflicted filters in the session
+)
 
 function Get-Fullname 
 {
@@ -26,25 +22,63 @@ function Get-Fullname
     {
         if (Test-Path $filename) 
         {
-        (Get-Item $fileName).FullName} 
+            (Get-Item $fileName).FullName
+        } 
+    }
+}
+
+
+#Check if the script is called from inside a git repo return -1 one on failure
+function isGit
+{
+    process
+    {
+        $isGit = git rev-parse --is-inside-work-tree
+        if (!$isGit)
+        {
+            echo "This script needs to be called from inside a git repo directory."
+            exit -1
         }
     }
+}
 
-function DownloadModelfromBranch
+#convert the "windows path to a git path - works with realtive and absolute path. return -1 one on failure
+function Get-GitMappedFilePaths  
 {
     param
     (
-        [Parameter(Mandatory = $true)][string] $filename,
-        [Parameter(Mandatory = $true)][string] $Branch,
-        [Parameter(Mandatory = $true)][string] $tempFilename
+        [Parameter(Mandatory = $true)][string] $filename
     )
     process
     {
-        $url = "https://github.com/LieberLieber/LemonTree.Automation.Workflows/raw/$Branch/$filename"
-        echo "Downloading from $url"
+        $gitFilename = git ls-files --full-name $filename
+        if ([string]::IsNullOrEmpty($gitFilename))
+        {
+            echo "This filename parameter doesn't point to an existing file inside The repo directory."
+            exit -1
+        }
+        return @($gitFilename);
+    }
+}
+
+#Get the files from the remotes by commitid via http and curl - no LFS or not LFS handling needed
+function Get-DownloadedFilename  
+{
+    param
+    (
+        [Parameter(Mandatory = $true)][string] $gitFilename,
+        [Parameter(Mandatory = $true)][string] $commitID
+    )
+    process
+    {
+        $gitUri =  git config --get remote.origin.url
+        $gitUri = $gitUri.Replace(".git","");
+        $stripPathFilename = $commitID + "_"+[System.IO.Path]::GetFileName($gitFilename)
+        $tempFilename = Join-Path -Path "$env:TEMP" -ChildPath "$stripPathFilename"
+        $url = "$gitUri/raw/$baseId/$gitFilename"
         while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
-        curl "$url" --output '$tempFilename' -L -k #-L follows the redirect to get the LFS file.
-    
+        curl "$url" --output "$tempFilename" -L -k # -s #-L follows the redirect to get the LFS file.
+        return @($tempFilename);
     }
 }
 
@@ -56,62 +90,85 @@ function Get-ModelRootIds
     )
     process
     {
-        $conn = New-Object System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source='$absoluteModel'") 
-        $conn.Open()
+        #Comment needed for 32 bit jet driver - 64 bit ACE is not all computers.
+        $script = Start-Job -ScriptBlock {       
+            #x64 db connection
+            #$conn = New-Object System.Data.OleDb.OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source='$absoluteModel'") 
 
-        $cmd = $conn.CreateCommand()
-        $cmd.CommandText ="select ea_guid from t_package where parent_id = 0"
-        $reader = $cmd.ExecuteReader()   
+            #x86 db connection
+            $conn = New-Object System.Data.OleDb.OleDbConnection("Provider=Microsoft.Jet.OLEDB.4.0; Data Source='$absoluteModel'") 
 
-        $results = @()
-        while ($reader.Read())
-        {
-            $row = @{}
-            for ($i = 0; $i -lt $reader.FieldCount; $i++)
+            $conn.Open()
+
+            $cmd = $conn.CreateCommand()
+            $cmd.CommandText ="select ea_guid from t_package where parent_id = 0"
+            $reader = $cmd.ExecuteReader()   
+
+            $results = @()
+            while ($reader.Read())
             {
-                $row[$reader.GetName($i)] = $reader.GetValue($i)
-            }
-            $results += new-object psobject -property $row  
+                $row = @{}
+                for ($i = 0; $i -lt $reader.FieldCount; $i++)
+                {
+                    $row[$reader.GetName($i)] = $reader.GetValue($i)
+                }
+                $results += new-object psobject -property $row  
                     
-        }
-        $conn.Close();
-
+            }
+            $conn.Close();
+        } -RunAs32
+        $script | Wait-Job | Receive-Job
         return @($results);
+        #Comment this in to run in x86 space with Jet Driver
     }
 }
 
-if ((Test-Path -Path $Model -PathType Leaf)) {
-     try {
-        #model exists we can party
-        echo "Before"
-        $absoluteModel = Get-Fullname($Model)
-        echo $Model
-        echo "after"
-        echo $absoluteModel
-        $tempFilename = "$Model"
-        echo $tempFilename
-        $absolutetempFilename = "$env:TEMP\$tempFilename"
-        echo $absolutetempFilename
-      
-        echo "Branch $Branch"
-        DownloadModelfromBranch $Model $Branch $absolutetempFilename
+#main script
+Echo "Model CherryPicking with LieberLieber LemonTree"
+#check if the script is called inside a repo return -1 one on failure
+isGit
 
-        $results = Get-ModelRootIds($absoluteModel);
-       
-        $results
+#convert the "windows path to a git path - works with realtive and absolute path. return -1 one on failure
+$gitFilename = Get-GitMappedFilePaths($filename)
+echo "filename parameter converted to git syntax $filename ==> $gitFilename"
 
-       &'C:\Program Files\LieberLieber\LemonTree\LemonTree.exe' -merge --base "$absolutetempFilename" --theirs "$absolutetempFilename" --mine "$absoluteModel" 
+#save the absolute path to the model before we change the working dir
+$absoluteFilename = Get-Fullname $filename
 
-     }
-     catch {
-         throw $_.Exception.Message
-     }
- }
-# If the file doesn't exists, show the message and do nothing.
- else {
-     Write-Host "Model doesn't exist!"
- }
+#set working directory temporaly to the root of the git repo
+$startDirectory = Get-Location
+$gitRootDir= git rev-parse --show-toplevel
+Set-Location $gitRootDir
 
-#Comment this in to run in x86 space with Jet Driver
-# } -RunAs32
-#'$script | Wait-Job | Receive-Job
+# Get and output date from 3 branches/commmits involved.
+$currentBranch = git branch --show-current
+$baseId = git merge-base "origin/$compareToBranch" $currentBranch
+$branchId = git log -n 1 "origin/$compareToBranch" --pretty=format:"%H"
+echo "Currently activ Branch: $currentBranch"
+echo "Base commit id: $baseId"
+echo "$compareToBranch commit id: $branchId"
+
+#Get the files from the remotes by commitid via http and curl - no LFS or not LFS handling needed
+echo "Start Downloading ..."
+$baseFileName = Get-DownloadedFilename $gitFilename $baseId
+$branchFileName = Get-DownloadedFilename $gitFilename $branchId
+Echo "Finished downloading."
+Echo "Base:   $baseFileName"
+Echo "Branch: $branchFileName"
+
+
+#getrootids for the merge hint from the current model.
+$results = Get-ModelRootIds($absoluteFilename);
+$results
+
+
+
+# Open LemonTree with Files.        
+$LemonTreeCommando = "-merge --base '$baseFileName' --theirs '$branchFileName' --mine '$absoluteFilename' --out '$absoluteFilename'"
+Echo "Starting LemonTree with $LemonTreeCommando"
+#&'C:\Program Files\LieberLieber\LemonTree\LemonTree.exe' -merge --base "$baseFileName" --theirs "$branchFileName" --mine "$absoluteFilename"
+
+
+Set-Location $startDirectory
+exit 0
+
