d1f25a477720fb487b01d27cf7c6b3fbd97719a1
danielsiegl <daniel.siegl@lieberlieber.com>
2022-04-29 14:00:23 +0200

    This way to get the base is very promising!

--- example-scripts/powershell/getFilefromGitBranch.ps1
+++ example-scripts/powershell/getFilefromGitBranch.ps1
@@ -1,7 +1,21 @@
-﻿$filename = "..\..\DemoModel.eapx"
+﻿git rev-parse --is-inside-work-tree
+
+$filename = "..\..\DemoModel.eapx"
+$gitFilename = git ls-files --full-name $filename
+echo "$filename ==> $gitFilename"
+
+#getabsolutefilename
+
 $compareToBranch ="main"
 
+$startDirectory = Get-Location
+$gitRootDir= git rev-parse --show-toplevel
+Set-Location $gitRootDir
+
+#mapit realtive to git repo.
+
 $currentBranch = git branch --show-current
+
 $baseId = git merge-base $compareToBranch $currentBranch
 echo $baseId
 $gitUri =  git config --get remote.origin.url
@@ -11,7 +25,9 @@ echo $gitUri
 $tempFilename = [System.IO.Path]::GetTempFileName() +".eapx"
 echo $tempFilename
 
+$url = "$gitUri/raw/$baseId/$gitFilename"
 echo "Downloading from $url"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
 curl "$url" --output "$tempFilename" -L -k #-L follows the redirect to get the LFS file.
 
+Set-Location $startDirectory
