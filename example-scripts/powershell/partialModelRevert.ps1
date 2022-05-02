1e32ff6a496d1134b1766352305b54337bc9ff5c
danielsiegl <daniel.siegl@lieberlieber.com>
2022-05-02 20:48:32 +0200

    Initial Commit

---
+++ example-scripts/powershell/partialModelRevert.ps1
@@ -0,0 +1,20 @@
+﻿$Model ="DemoModel.eapx"
+$commitId = git rev-parse HEAD
+Echo "Head CommitID $commitID"
+$fileReference = $commitID+":"+$Model
+Echo $fileReference
+
+            $pointer = git cat-file blob $fileReference
+            Echo "Head Pointer $pointer"
+            $sha = ($pointer[1] -split(":"))[1]
+          if($sha -ne $null){
+            $shaPart1 = $sha.Substring(0,2)
+            $shaPart2 = $sha.Substring(2,2)
+            echo "Model SHA: $sha"
+            git cat-file --filters $fileReference | Out-Null
+            copy ".git\lfs\objects\$shaPart1\$shaPart2\$sha" "HEAD_$Model"
+            echo "::set-output name=result::downloaded"
+          }
+          else{
+            echo "::set-output name=result::notFound"
+          }
