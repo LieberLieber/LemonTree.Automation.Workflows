05d8a07171e37b614e98dc95372e6aa6b3e90bd2
danielsiegl <41949368+danielsiegl@users.noreply.github.com>
2022-05-11 09:00:32 +0000

    Incquery Labs Integration (#131)
    * Create validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Update validateIncQuery.yml
    
    Ignore warning
    
    * Update validateIncQuery.yml
    
    * Update DemoModel.eapx
    
    Fixed one of the warnings
    
    * Revert "Update DemoModel.eapx"
    
    This reverts commit e1c1941108b386793f25f76a39d7af300cefc1d2.
    
    * Update validateIncQuery.yml
    
    Only create a PR Comment if a PR Exists
    
    * Update validateIncQuery.yml
    
    fixed Typo
    
    * Update DemoModel.eapx
    
    should have an error
    
    * Better Diff Handling
    
    * How to get a base - based on checkin comment
    
    * Upload Session File
    
    * This is fixed

---
+++ example-scripts/powershell/DiffReport.ps1
@@ -0,0 +1,33 @@
+﻿$output = &"C:\Program Files\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe" diff --theirs C:\PCS\Models\LemonTree.PCS.Demo\3-demo-nizam\model.qeax --mine C:\PCS\Models\LemonTree.PCS.Demo\8-demo3\model.qeax
+
+$diffResult = ""
+ForEach ($line in $($output -split "`r`n"))
+{
+    if ($line.EndsWith("potentially different elements."))
+    {
+        #ignore
+    }
+    elseif ($line.EndsWith(" different elements."))
+    {
+        Echo $Line
+        $diffResult += $line
+        $diffResult += "`r`n"
+    }
+    elseif ($line.StartsWith("Found conflicts:"))
+    {
+        if ($line.StartsWith("Found conflicts: 0"))
+        {
+            #ignore if there is 0 conflicts
+        }
+        else
+        {
+            Echo $Line
+            $diffResult += $line
+            $diffResult += "`r`n"
+        }
+
+    }
+
+}
+
+Echo "Results:`r`n $diffresult"
