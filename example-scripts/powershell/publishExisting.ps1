da3e94bc2a4d83cd2765e07913090016768608ee
danielsiegl <daniel.siegl@lieberlieber.com>
2022-05-30 15:56:37 +0200

    fixed

--- example-scripts/powershell/publishExisting.ps1
+++ example-scripts/powershell/publishExisting.ps1
@@ -1,4 +1,21 @@
 #Powershell sees curly brackets as codeblocks so use quotations marks "{}" or just write your guids without them
-C:\"Program Files"\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe publish --model model.eap --packagedirectory .\repo --componentguids "{c2791d98-4982-d2dd-449c-271ac0dbf1c2}","f7b4a94e-422c-94ee-1d44-ed33655e8d8e" --releasenotes "my notes"
+$componentGuids =""
+
+$componentFiles = Get-ChildItem *.mpms
+$count = 0
+foreach ($componentFile in $componentFiles)
+{
+  if ($count -ne 0)
+  {
+    $componentGuids+=","
+  }
+  #take the guid via substring from e.g "Customer A-46e3f67f-4e87-c98e-129a-049eb063405f"
+  $newGuid = $componentFile.BaseName.Substring($componentFile.BaseName.Length-36,36);
+  $componentGuids +=  $newGuid
+  $count++
+}
+
+echo "Exporting Components: $componentGuids"
+C:\"Program Files"\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe publish --model model.eapx --packagedirectory .\ --componentguids "$componentGuids" --releasenotes "my notes"
 echo Exitcode $LASTEXITCODE
 pause
