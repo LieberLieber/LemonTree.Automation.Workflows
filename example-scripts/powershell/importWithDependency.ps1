bab342e3e849749c0a4a96e99f5696251d239399
danielsiegl <daniel.siegl@lieberlieber.com>
2022-06-01 23:17:41 +0200

    Create importWithDependency.ps1

---
+++ example-scripts/powershell/importWithDependency.ps1
@@ -0,0 +1,30 @@
+﻿$mpmsfile ="C:\LemonTreeTestData\publishExistingDemo\Customer A-46e3f67f-4e87-c98e-129a-049eb063405f.mpms"
+Echo ""
+$componentFileName =""
+foreach($line in [System.IO.File]::ReadLines($mpmsfile))
+{
+    if ($line.StartsWith("//{"))
+    {
+       $jsonContent = $line.Substring(2,$line.Length-2)| ConvertFrom-Json
+       $count = 0
+       foreach ($dependency in $jsonContent.Dependencies)
+       {
+        if ($count -ne 0)
+        {
+          $componentFileName +=","  
+        }
+        $count++
+
+        $filename =""
+        $filename +=$dependency.name
+        $filename +="-"
+        $filename +=$dependency.target
+        $filename +=".mpms"
+        
+        $componentFileName +=$filename
+        
+       }
+       break
+    }
+}
+$componentFileName
