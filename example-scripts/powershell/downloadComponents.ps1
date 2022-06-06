31a2c9328befea269ede31402d330900623ac929
danielsiegl <41949368+danielsiegl@users.noreply.github.com>
2022-06-06 21:28:46 +0200

    141 create script that only exports already exisiting mpms (#144)
    * developed script
    
    * fixed
    
    * Create importWithDependency.ps1
    
    * Update importWithDependency.ps1
    
    updated string handling - finally know how I can work arround some of the weird powershell stuff
    
    * Update importWithDependency.ps1
    
    integrated LTA and removed curly brackets.
    
    * update

--- example-scripts/powershell/downloadComponents.ps1
+++ example-scripts/powershell/downloadComponents.ps1
@@ -3,6 +3,7 @@ foreach ($url in $json.items.assets.downloadUrl) {
   $url = $url.Insert(4,"s")
   $file = $url.Substring($url.lastIndexOf('/') + 1)
   Write-Host $file
+  ECHO 4url
   $content = ((Invoke-RestMethod -Uri $url -Method Get) -replace "﻿")
   [IO.File]::WriteAllLines($file, $content)
 }
