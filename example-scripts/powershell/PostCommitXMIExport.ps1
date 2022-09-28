6dcb7d7e6f75338921dfbd728f34c8a32ad67105
danielsiegl <daniel.siegl@lieberlieber.com>
2022-09-28 21:17:31 +0200

    Test

--- example-scripts/powershell/PostCommitXMIExport.ps1
+++ example-scripts/powershell/PostCommitXMIExport.ps1
@@ -4,7 +4,6 @@ $ea = New-Object -ComObject "EA.Repository" -Strict
 Echo "Created EA"
 $ea.OpenFile("C:\github\LemonTree.Automation.Workflows\DemoModel.eapx")
 
-
 $theProject = $ea.GetProjectInterface();
 $theProject.ExportPackageXMI("{FA69A423-37E5-4c35-B982-0849B8820AB3}",22,0,-1,0,0,"C:\github\LemonTree.Automation.Workflows\export.xmi");
 $ea.CloseFile()
