532091f6fbaf81ffc4ea9c39949408582ded8c4e
danielsiegl <daniel.siegl@lieberlieber.com>
2022-09-29 17:58:12 +0200

    Test

--- example-scripts/powershell/post-commit.ps1
+++ example-scripts/powershell/post-commit.ps1
@@ -1,5 +1,5 @@
 Write-Output "post-commit publish of XMI to Nexus"
-#$curl = "C:\Program Files\Git\mingw64\bin\curl.exe"
+$curl = "C:\Program Files\Git\mingw64\bin\curl.exe"
 $mypath = $MyInvocation.MyCommand.Path
 Write-Output "Create EA"
 $ea = New-Object -ComObject "EA.Repository" -Strict
@@ -8,12 +8,11 @@ $modelfile = Join-Path -Path $repoPath  -ChildPath "DemoModel.eapx"
 $gitcommitId = git rev-parse HEAD
 $xmiFileName = "$gitcommitId.xmi"
 $xmiFile = Join-Path -Path $repoPath  -ChildPath "$xmiFileName"
+Write-Output "Created EA Instance"
 Write-Output "Model File : $modelfile"
 Write-Output "XMI File : $xmiFile"
-
-Write-Output "Created EA"
+Write-Output "Open Model"
 $ea.OpenFile($modelfile)
-
 $theProject = $ea.GetProjectInterface();
 Write-Output "Running XMI Export"
 $result = $theProject.ExportPackageXMI("{FA69A423-37E5-4c35-B982-0849B8820AB3}",22,0,-1,0,0,$xmiFile);
@@ -28,5 +27,5 @@ $login = (New-Object PSCredential "username",$SecureCredential).GetNetworkCreden
 $targetUrl = "https://nexus.lieberlieber.com/repository/xmi/"
 Write-Output "Uploading $xmiFile to Nexus: $targetUrl"
 while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
-&curl "-u$login" -T "$xmiFile" "$targetUrl"
+&$curl "-u$login" -T "$xmiFile" "$targetUrl"
 Write-Output "Uploaded to Nexxus" 
