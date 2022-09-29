90e28c45999ed3bf8d28050c22d8bb7a56e62cb7
danielsiegl <daniel.siegl@lieberlieber.com>
2022-09-29 23:07:22 +0200

    Download xmi from Nexus

---
+++ example-scripts/powershell/DownloadPostCommitXMI.ps1
@@ -0,0 +1,12 @@
+$gitcommitId = git rev-parse HEAD
+$json = Invoke-RestMethod -Uri https://nexus.lieberlieber.com/service/rest/v1/components?repository=xmi -Method Get
+foreach ($url in $json.items.assets.downloadUrl) {
+  $url = $url.Insert(4,"s")
+  $file = $url.Substring($url.lastIndexOf('/') + 1)
+  if($file.StartsWith($gitcommitId))
+  {
+    Write-Host $file
+    $content = ((Invoke-RestMethod -Uri $url -Method Get) -replace "﻿")
+    [IO.File]::WriteAllLines($file, $content)
+  }
+}
