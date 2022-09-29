c0c00cbff76ea1b758c1184d2c564de9606ff347
danielsiegl <daniel.siegl@lieberlieber.com>
2022-09-29 23:24:36 +0200

    Comment

--- example-scripts/powershell/DownloadPostCommitXMI.ps1
+++ example-scripts/powershell/DownloadPostCommitXMI.ps1
@@ -1,10 +1,13 @@
 #this script downloads xmi files from the artifact storage to be used by pipeline tools.
+#typically it will run on an build agent
 
 $gitcommitId = git rev-parse HEAD
 $json = Invoke-RestMethod -Uri https://nexus.lieberlieber.com/service/rest/v1/components?repository=xmi -Method Get
 foreach ($url in $json.items.assets.downloadUrl) {
   $url = $url.Insert(4,"s")
   $file = $url.Substring($url.lastIndexOf('/') + 1)
+
+  #identifies files from this commitId
   if($file.StartsWith($gitcommitId))
   {
     Write-Host $file
