be3d5c19b2e760f6a1fb60a8b1edc2fe42a34673
danielsiegl <daniel.siegl@lieberlieber.com>
2022-09-29 13:00:03 +0200

    added a temporary solution to persist the password in a file

---
+++ example-scripts/powershell/CreateNexusCredentials.ps1
@@ -0,0 +1,7 @@
+﻿param
+(
+        [Parameter(Mandatory = $true, HelpMessage="Enter: 'Username:Password'")][string] $Credential
+)
+$repoPath = git rev-parse --show-toplevel
+$file = Join-Path -Path $repoPath  -ChildPath "..\nexus.inf"
+$Credential | ConvertTo-SecureString -AsPlainText -Force | ConvertFrom-SecureString | Out-File "$file"
