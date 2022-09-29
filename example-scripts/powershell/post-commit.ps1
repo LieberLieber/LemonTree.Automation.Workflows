18e4f61b2784d11bf940f8d739aa23fd6dc510ee
danielsiegl <daniel.siegl@lieberlieber.com>
2022-09-29 20:38:33 +0200

    Handling for multiple modelroots

--- example-scripts/powershell/post-commit.ps1
+++ example-scripts/powershell/post-commit.ps1
@@ -2,34 +2,53 @@
 #It will publish an XMI file to in our case Nexus to be used by pipelinetools that handle EA XMI
 #to embedded this script in your hook file simply add the next line to the post-commit file.
 #c:/Windows/System32/WindowsPowerShell/v1.0/powershell.exe -ExecutionPolicy RemoteSigned -File 'example-scripts\powershell\post-commit.ps1'
+
+#prepare environment
 Write-Output "post-commit publish of XMI to Nexus"
-$mypath = $MyInvocation.MyCommand.Path
-Write-Output "Create EA"
-$ea = New-Object -ComObject "EA.Repository" -Strict
 $repoPath = git rev-parse --show-toplevel
 $modelfile = Join-Path -Path $repoPath  -ChildPath "DemoModel.eapx"
 $gitcommitId = git rev-parse HEAD
-$xmiFileName = "$gitcommitId.xmi"
-$xmiFile = Join-Path -Path $repoPath  -ChildPath "$xmiFileName"
+
+#prepare upload to Artifact Provider
+$file =  Join-Path -Path $repoPath  -ChildPath "..\nexus.inf"
+$file = [System.IO.Path]::GetFullPath($file)
+#not the savest way on earth but a pragmatic solution for the sake of this demo
+$SecureCredential = Get-Content "$file" | ConvertTo-SecureString
+$login = (New-Object PSCredential "username",$SecureCredential).GetNetworkCredential().Password
+$targetUrl = "https://nexus.lieberlieber.com/repository/xmi/"
+
+#start EA and load the Model
+Write-Output "Create EA"
+$ea = New-Object -ComObject "EA.Repository" -Strict
 Write-Output "Created EA Instance"
 Write-Output "Model File : $modelfile"
-Write-Output "XMI File : $xmiFile"
 Write-Output "Open Model"
 $ea.OpenFile($modelfile)
 $theProject = $ea.GetProjectInterface();
+
+#XMI Export one file per Model Root
 Write-Output "Running XMI Export"
-$result = $theProject.ExportPackageXMI("{FA69A423-37E5-4c35-B982-0849B8820AB3}",22,0,-1,0,0,$xmiFile);
+foreach($model in $ea.models) 
+{ 
+    $modelName = $model.Name
+    $packageGuid = $model.PackageGUID
+    #we don't want to upload the LemonTree.Components Stuff
+    if (-Not $modelName.Equals("MPMS Specification and Configuration"))
+    {
+        $xmiFileName = "$gitcommitId-$modelName.xmi"
+        $xmiFile = Join-Path -Path $repoPath  -ChildPath "$xmiFileName"
+        Write-Output "Exporting $modelName with $packageGuid  to $xmiFile"
+        $result = $theProject.ExportPackageXMI("$packageGuid",22,0,-1,0,0,$xmiFile);
+         
+        Write-Output "Uploading $xmiFile to Nexus: $targetUrl"
+        &curl.exe "-u$login" -T "$xmiFile" "$targetUrl"
+    }
+}
+
 Write-Output "Finsihed XMI Export"
 $ea.CloseFile()
 $ea.Exit()
 Write-Output "Disposed EA"
-$file =  Join-Path -Path $repoPath  -ChildPath "..\nexus.inf"
-$file = [System.IO.Path]::GetFullPath($file)
-$SecureCredential = Get-Content "$file" | ConvertTo-SecureString
-$login = (New-Object PSCredential "username",$SecureCredential).GetNetworkCredential().Password
-$targetUrl = "https://nexus.lieberlieber.com/repository/xmi/"
-Write-Output "Uploading $xmiFile to Nexus: $targetUrl"
-while (Test-Path Alias:curl) {Remove-Item Alias:curl} #remove the alias binding from curl to Invoke-WebRequest
-Write-Output $login
-&curl.exe "-u$login" -T "$xmiFile" "$targetUrl"
-Write-Output "Upload to Nexus"
+
+
+
