56f0355b26a95947bcb9a6e25f3ec3fcb9fe47fb
Daniel Siegl <daniel.siegl@lieberlieber.com>
2023-02-28 18:31:38 +0100

    SCript Starts LemonTree now

--- example-scripts/powershell/mpmsMerge.ps1
+++ example-scripts/powershell/mpmsMerge.ps1
@@ -1,34 +1,47 @@
-#the purpose of this script is to replace LemonTree.Starter.exe for .mpms files.
-#LemonTreeStarter will be enganged once three EA Models have been created useing LTA
+# the purpose of this script is to replace LemonTree.Starter.exe for .mpms files.
+# LemonTreeStarter will be enganged once three EA Models have been created useing LTA
 
+# same parameters as LemonTree Starter
+# --out=${mergedFile} --base=${baseFile} --mine=${rightFile} --theirs=${leftFile}
+param
+(
+    [Parameter(Mandatory = $false)][string] $out = "C:\LemonTreeTestData\MPMS Merge\merge\SysML-22b116db-59ea-a4fa-c52a-cc163a9f56bc.mpms",
+    [Parameter(Mandatory = $false)][string] $base ="C:\LemonTreeTestData\MPMS Merge\base\SysML-22b116db-59ea-a4fa-c52a-cc163a9f56bc.mpms",
+    [Parameter(Mandatory = $false)][string] $mine ="C:\LemonTreeTestData\MPMS Merge\mine\SysML-22b116db-59ea-a4fa-c52a-cc163a9f56bc.mpms",
+    [Parameter(Mandatory = $false)][string] $theirs ="C:\LemonTreeTestData\MPMS Merge\theirs\SysML-22b116db-59ea-a4fa-c52a-cc163a9f56bc.mpms"
+)
+
+#tool environment - adapt as needed
 $LTAToolPath = "C:\Program Files\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe"
-$LTSToolPath = "C:\Program Files\LieberLieber\LemonTree\LemonTree.Starter.exe"
-
-$dataFolder= "C:\LemonTreeTestData\MPMS Merge"
-$mpmsfile = "SysML-22b116db-59ea-a4fa-c52a-cc163a9f56bc.mpms"
+$LTSToolPath = "C:\Program Files\LieberLieber\LemonTree\LemonTree.exe"
 $EABaseModel = "C:\Program Files (x86)\Sparx Systems\EA\EABase.qea"
+
+#define names of tempmodels
 $BaseModel = "base.qeax"
 $MineModel = "mine.qeax"
 $TheirsModel = "theirs.qeax"
 $ResultModel = "result.qeax"
 
 
-
+#create template models
 Copy-Item $EABaseModel $BaseModel
+&$LTAToolPath Import --Model $BaseModel --Components $base
+
 Copy-Item $EABaseModel $MineModel
+&$LTAToolPath Import --Model $MineModel --Components $mine
+
 Copy-Item $EABaseModel $TheirsModel
+&$LTAToolPath Import --Model $TheirsModel --Components $theirs
 
-$componentFile = "$dataFolder\base\$mpmsfile"
-Write-Output $componentFile 
-&$LTAToolPath Import --Model $BaseModel --Components $componentFile 
+#use lemontree to merge the models.
+$command = "--merge=auto --out=$ResultModel --base=$BaseModel --mine=$MineModel --theirs=$TheirsModel --singleSessionOnly"
+$myprocss = Start-Process $LTSToolPath -PassThru -ArgumentList $command
+$myprocss.WaitForExit()
+Write-Output "LemonTree is finished"
 
-$componentFile = "$dataFolder\mine\$mpmsfile"
-Write-Output $componentFile 
-&$LTAToolPath Import --Model $MineModel --Components $componentFile 
+#we could check for the result model here? for now we go forward
 
-$componentFile = "$dataFolder\theirs\$mpmsfile"
-Write-Output $componentFile 
-&$LTAToolPath Import --Model $TheirsModel --Components $componentFile 
+#guid extract from Filename? '{22b116db-59ea-a4fa-c52a-cc163a9f56bc}
 
-&$LTSToolPath merge --out $ResultModel --base $BaseModel --mine $MineModel --theirs $TheirsModel
+#extract MPMS to the location of $out
 
