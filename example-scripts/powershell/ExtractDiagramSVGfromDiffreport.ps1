8a84f6720c59ce6e061b9928afcb9b04a168126c
Daniel Siegl <daniel.siegl@lieberlieber.com>
2023-11-07 09:03:30 +0100

    Implemented export of diagrams to SVG

---
+++ example-scripts/powershell/ExtractDiagramSVGfromDiffreport.ps1
@@ -0,0 +1,27 @@
+﻿# Copyright (c) LieberLieber Softwar GmbH
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#This script will export the mine diagrams from an xml diffreport into a SVG file
+
+param (
+    [string]$xmlFilePath = "DiffReport.xml"
+)
+
+$xmlContent = Get-Content -Path $xmlFilePath | Out-String
+select-xml -Content $xmlContent -xpath "//diagramPictures/diagramPicture" | foreach {
+    $diagramGuid = $_.node.guid
+    if($diagramGuid ){
+        select-xml -Content $xmlContent -xpath "//diagramPictures/diagramPicture[@guid='$diagramGuid']/diagramPictureB" | foreach {
+            #check if cdata exists
+            if ($_.node.InnerText)
+            {
+                $qualifiedName = $_.node.qualifiedName
+                $fileGuid = $diagramGuid.Replace('{','').Replace('}','')
+                $filename = "$fileGuid.svg"
+                write-output "$qualifiedName ==> $filename"
+                $_.node.InnerText | %{[Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($_))}|Set-Content -Encoding UTF8 -Path $filename
+            }
+        }
+    }}
