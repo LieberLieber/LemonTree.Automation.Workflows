cd2bf2ab88bf20aa49141e188596298125f9a3de
Daniel Siegl <41949368+danielsiegl@users.noreply.github.com>
2023-11-13 16:10:12 +0100

    201 create action with svg export and git commit of diagrams (#202)
    * LemonTree.Automation SVG Update
    
    * Added Action that will Auto Update SVG Diagrams in Repo
    
    * LemonTree.Automation SVG Update
    
    * Revert "LemonTree.Automation SVG Update"
    
    This reverts commit 35552ec7eb8e50d0f48ce673619834c46a10444c.
    
    * Updated Script to checkin only SVG
    
    * Model Update
    
    * Updated Script
    
    * LemonTree.Automation SVG Update
    
    ---------
    
    Co-authored-by: LemonTree.Automation <support@lieberlieber.com>

---
+++ example-scripts/powershell/UpdateSVGDIagramsinRepo.ps1
@@ -0,0 +1,30 @@
+﻿# Copyright (c) LieberLieber Software GmbH
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#This script will update the "mine" diagrams from a LemonTree xml diffreport into SVG files
+
+#Check if the Folder Exists 
+$folderPath = ".\svg"
+if (Test-Path $folderPath) {
+    Write-Output "Folder $folderPath exists"
+} else {
+    Write-Output "Folder $folderPath does not exist, we create it "
+    New-Item -ItemType Directory -Force -Path $folderPath
+}
+
+#delete all SVG files from the Folder - so deletion of diagrams is also reflected in git. existing diagrams will be recreated anyway.
+$filesSVG =	$folderPath+"\*.svg"
+Write-Output "deleting files $filesSVG"
+Remove-Item "$filesSVG"
+
+#create SVG Files 
+&'C:\Program Files\LieberLieber\LemonTree.Automation\LemonTree.Automation.exe' SVGExport --Model .\DemoModel.eapx --DiagramDirectory '.\svg'
+
+#git commit
+git add -A
+git commit -m "LemonTree.Automation SVG Update"
+
+#git push
+#git push
