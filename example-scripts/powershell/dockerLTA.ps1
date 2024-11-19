1c5277aa5a88d2a5a27a3e02d76795ffd0988207
Daniel Siegl <daniel.siegl@lieberlieber.com>
2024-11-19 14:56:57 +0100

    [#225] Create and example to show how to use LemonTree.Automation Docker container for pipeline tasks.

--- example-scripts/powershell/dockerLTA.ps1
+++ example-scripts/powershell/dockerLTA.ps1
@@ -6,11 +6,23 @@
 #this is not a finished script - it is just the basic info to get you started using LemonTree.Automation deployed as a Docker Container into your pipeline.
 #sample models can be found on computers with LemonTree Desktop installed: C:\ProgramData\LieberLieber\LemonTree\Examples
 
+
+$ltaLicenses = "$(Get-Location)/licenses"
+$ltaData ="$(Get-Location)/data"
+
+#prepare mapping directories for docker mapping
+New-Item -ItemType Directory -Force -Path "$ltaLicenses"
+New-Item -ItemType Directory -Force -Path "$ltaData"
+
+#copy my locallicense to the folder - pls use yours as they fit
+Copy-Item -Path "C:\tmp\license000_ANY.lic" -Destination "$ltaLicenses" -Force 
+Copy-Item -Path "C:\ProgramData\LieberLieber\LemonTree\Examples\A.qeax" -Destination "$ltaData" -Force 
+Copy-Item -Path "C:\ProgramData\LieberLieber\LemonTree\Examples\B.qeax" -Destination "$ltaData" -Force 
+Copy-Item -Path "C:\ProgramData\LieberLieber\LemonTree\Examples\Base.qeax" -Destination "$ltaData" -Force 
+
+docker stop ltacli
 docker rm ltacli
-docker run -itd --name ltacli `
-    -v "$(Get-Location)\data:/data" ` 
-    -v "$(Get-Location)\licenses:/app/licenses" `
-    nexus.lieberlieber.com:5000/lieberlieber/lemontree.automation:latest
+docker run -itd --name ltacli -v "${ltaData}:/data" -v "${ltaLicenses}:/app/licenses" nexus.lieberlieber.com:5000/lieberlieber/lemontree.automation:latest
 	
 docker exec -it ltacli ./lemontree.automation diff --license /app/licenses/ --base /data/Base.qeax --theirs /data/A.qeax --mine /data/B.qeax
  
