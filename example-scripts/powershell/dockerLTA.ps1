7f0258716f2828b10eab03089d0cbb546ed8600b
Daniel Siegl <41949368+danielsiegl@users.noreply.github.com>
2024-11-19 06:34:26 -0800

    225 create and example to show how to use lemontreeautomation docker container for pipeline tasks (#226)
    * [#225] Create and example to show how to use LemonTree.Automation Docker container for pipeline tasks.
    
    * Checkin a first boilerplate example.
    
    * Added more documentation
    
    * [#225] Create and example to show how to use LemonTree.Automation Docker container for pipeline tasks.
    
    * [#225] Create and example to show how to use LemonTree.Automation Docker container for pipeline tasks.
    
    Added environment disclaimer
    
    * Created sample scritp with Docker LTA

---
+++ example-scripts/powershell/dockerLTA.ps1
@@ -0,0 +1,30 @@
+# Copyright (c) LieberLieber Software GmbH
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+# file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+#this is not a finished script - it is just the basic info to get you started using LemonTree.Automation deployed as a Docker Container into your pipeline.
+#sample models can be found on computers with LemonTree Desktop installed: C:\ProgramData\LieberLieber\LemonTree\Examples
+
+#as is this script is built to run on a Windows Desktop with Docker Desktop installed.
+
+$ltaLicenses = "$(Get-Location)/licenses"
+$ltaData ="$(Get-Location)/data"
+
+#prepare mapping directories for docker mapping
+New-Item -ItemType Directory -Force -Path "$ltaLicenses"
+New-Item -ItemType Directory -Force -Path "$ltaData"
+
+#copy my locallicense to the folder - pls use yours as they fit
+Copy-Item -Path "C:\tmp\license000_ANY.lic" -Destination "$ltaLicenses" -Force 
+Copy-Item -Path "C:\ProgramData\LieberLieber\LemonTree\Examples\A.qeax" -Destination "$ltaData" -Force 
+Copy-Item -Path "C:\ProgramData\LieberLieber\LemonTree\Examples\B.qeax" -Destination "$ltaData" -Force 
+Copy-Item -Path "C:\ProgramData\LieberLieber\LemonTree\Examples\Base.qeax" -Destination "$ltaData" -Force 
+
+docker stop ltacli
+docker rm ltacli
+docker run -itd --name ltacli -v "${ltaData}:/data" -v "${ltaLicenses}:/app/licenses" nexus.lieberlieber.com:5000/lieberlieber/lemontree.automation:latest
+	
+docker exec -it ltacli ./lemontree.automation diff --license /app/licenses/ --base /data/Base.qeax --theirs /data/A.qeax --mine /data/B.qeax
+ 
+
